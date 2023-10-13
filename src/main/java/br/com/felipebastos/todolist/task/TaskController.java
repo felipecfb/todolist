e3d92bd27c504e8e7841e4520eb08391edf76847
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-10-13 16:06:00 -0300

    feat: adding an route to update an task

--- src/main/java/br/com/felipebastos/todolist/task/TaskController.java
+++ src/main/java/br/com/felipebastos/todolist/task/TaskController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,5 +54,15 @@ public class TaskController {
     var tasks = this.taskRepository.findByUserId((UUID) userId);
 
     return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+
+    taskModel.setUserId((UUID) userId);
+    taskModel.setId(id);
+    
+    return this.taskRepository.save(taskModel);
   }
 }
