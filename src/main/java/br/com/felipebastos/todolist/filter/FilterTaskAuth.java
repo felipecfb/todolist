f7585fa18d9c20367b29532b3874eb298161e275
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-10-13 11:33:52 -0300

    feat: list tasks by user id

--- src/main/java/br/com/felipebastos/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/felipebastos/todolist/filter/FilterTaskAuth.java
@@ -49,7 +49,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
         if (passwordVerify.verified) {
-          request.setAttribute("idUser", user.getId());
+          request.setAttribute("userId", user.getId());
           filterChain.doFilter(request, response);
         } else {
           response.sendError(401);
